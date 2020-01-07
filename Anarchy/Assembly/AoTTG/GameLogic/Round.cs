aadeedae131817998737d8596c631c7911fe29b0
Order <43047529+Orrder@users.noreply.github.com>
2020-01-07 11:03:12 +0300

    Update Round.cs

--- Anarchy/Assembly/AoTTG/GameLogic/Round.cs
+++ Anarchy/Assembly/AoTTG/GameLogic/Round.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
-
-namespace GameLogic
+﻿namespace GameLogic
 {
     internal class Round
     {
-        public float GameEndCD { get; set; } = 10f;
-        public float GameEndTimer { get; set; } = 10f;
-        public bool GameStart { get; set; }
-        public bool IsLosing { get; set; }
-        public bool IsWinning { get; set; }
-        public float Time { get; set; } = 0f;
-        public int TeamWinner { get; set; }
-        public int Wave { get; set; } = 1;
+        public float GameEndCD = 10f;
+        public float GameEndTimer = 10f;
+        public bool GameStart;
+        public bool IsLosing;
+        public bool IsWinning;
+        public float Time = 0f;
+        public int TeamWinner;
+        public int Wave = 1;
 
         public Round()
         {
@@ -23,11 +17,11 @@ namespace GameLogic
 
         public void OnLateUpdate()
         {
-            float unscaled = UnityEngine.Time.deltaTime;
-            Time += unscaled;
-            if(IN_GAME_MAIN_CAMERA.GameType == GameType.Multi && (IsWinning || IsLosing))
+            float time = UnityEngine.Time.deltaTime;
+            Time += time;
+            if (IN_GAME_MAIN_CAMERA.GameType == GameType.Multi && (IsWinning || IsLosing))
             {
-                GameEndCD -= UnityEngine.Time.deltaTime;
+                GameEndCD -= time;
             }
         }
 
