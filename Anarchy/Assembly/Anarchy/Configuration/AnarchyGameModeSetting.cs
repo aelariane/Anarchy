2093ac7de088fe38f2b07cc6e8d1e27a6da579ce
Order <43047529+Orrder@users.noreply.github.com>
2020-01-11 21:48:48 +0300

    Create AnarchyGameModeSetting.cs

---
+++ Anarchy/Assembly/Anarchy/Configuration/AnarchyGameModeSetting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Anarchy.Configuration
+{
+    public class AnarchyGameModeSetting : GameModeSetting
+    {
+        public AnarchyGameModeSetting(string key) : this(key, -1, null, null) { }
+
+        public AnarchyGameModeSetting(string key, int sel) : this(key, sel, null, null) { }
+
+        public AnarchyGameModeSetting(string key, float[] floats) : this(key, -1, floats, null) { }
+
+        public AnarchyGameModeSetting(string key, int[] ints) : this(key, -1, null, ints) { }
+
+        public AnarchyGameModeSetting(string key, int sel, float[] floats) : this(key, sel, floats, null) { }
+
+        public AnarchyGameModeSetting(string key, int sel, int[] ints) : this(key, sel, null, ints) { }
+
+        public AnarchyGameModeSetting(string key, int selection, float[] floats, int[] ints) : base(key, selection, floats, ints)
+        {
+            RemoveCallback(RCSettingCallback);
+            AddCallback(AnarchySettingCallback);
+        }
+
+        public static void AnarchySettingCallback(GameModeSetting set, bool state, bool received)
+        {
+            if (PhotonNetwork.IsMasterClient && !received)
+            {
+                PhotonPlayer[] targets = PhotonPlayer.GetNotAnarchyUsers();
+                if (targets.Length <= 0)
+                {
+                    return;
+                }
+                FengGameManagerMKII.FGM.BasePV.RPC("Chat", targets, new object[] { set.ToString(false), string.Empty });
+            }
+        }
+    }
+}
