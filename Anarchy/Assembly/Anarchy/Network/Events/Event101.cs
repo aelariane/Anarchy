26af83bf453501d39de901ad9e47105e45c47ea5
Order <43047529+Orrder@users.noreply.github.com>
2020-01-12 11:39:05 +0300

    Added checks

--- Anarchy/Assembly/Anarchy/Network/Events/Event101.cs
+++ Anarchy/Assembly/Anarchy/Network/Events/Event101.cs
@@ -13,6 +13,18 @@ namespace Anarchy.Network.Events
 
         public bool CheckData(EventData data, PhotonPlayer sender, out string reason)
         {
+            Hashtable parameters = data[245] as Hashtable;
+            if (parameters != null)
+            {
+                if (parameters[101] is bool)
+                {
+                    sender.ModName = "[[000000]RedSkies[-]]";
+                }
+                else if (parameters[102] is bool)
+                {
+                    sender.ModName = $"[00FFFF][Cyan[CCCCDD]({parameters[104] as string})[-][-]]";
+                }
+            }
             reason = "";
             return true;
         }
