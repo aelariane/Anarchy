a4d8a090ab39b84e60c13b8d5b35649a05cb24bd
Order <43047529+Orrder@users.noreply.github.com>
2020-05-24 11:05:34 +0300

    Update Labels.cs

--- Anarchy/Assembly/Optimization/Labels.cs
+++ Anarchy/Assembly/Optimization/Labels.cs
@@ -1,43 +1,29 @@
 ﻿using UnityEngine;
 using static Optimization.Caching.Colors;
+using Anarchy;
 
 namespace Optimization
 {
     internal class Labels
     {
         internal static Font Font;
-        private static TextMesh bottomRight;
-        private static TextMesh center;
-        private static TextMesh topCenter;
-        private static TextMesh topLeft;
-        private static TextMesh topRight;
-        private static TextMesh networkStatus;
-        private static TextMesh version;
+        private static HUDLabel bottomRight = new HUDLabel("LabelInfoBottomRight", 32, TextAnchor.LowerRight, white, TextAlignment.Right);
+        private static HUDLabel center = new HUDLabel("LabelInfoCenter", 32, TextAnchor.LowerCenter, white,  TextAlignment.Center);
+        private static HUDLabel topCenter = new HUDLabel("LabelInfoTopCenter", 32, TextAnchor.UpperCenter, white,  TextAlignment.Center);
+        private static HUDLabel topLeft = new HUDLabel("LabelInfoTopLeft", 30, TextAnchor.UpperLeft, white, TextAlignment.Left, FontStyle.Bold);
+        private static HUDLabel topRight = new HUDLabel("LabelInfoTopRight", 28, TextAnchor.UpperRight, white,  TextAlignment.Right);
+        private static HUDLabel networkStatus = new HUDLabel("LabelNetworkStatus", 32, TextAnchor.UpperLeft, white, TextAlignment.Left);
+        private static HUDLabel version = new HUDLabel("VERSION", 30, TextAnchor.MiddleCenter, white, TextAlignment.Center);
 
         #region Labels
         public static string BottomRight
         {
             get
             {
-                if(bottomRight != null)
-                {
-                    return bottomRight.text;
-                }
-                bottomRight = CreateLabel("LabelInfoBottomRight", 32, TextAnchor.LowerRight, white, Font, TextAlignment.Right);
-                if (bottomRight == null)
-                    return "";
                 return bottomRight.text;
             }
             set
             {
-                if(bottomRight == null)
-                {
-                    bottomRight = CreateLabel("LabelInfoBottomRight", 32, TextAnchor.LowerRight, white, Font, TextAlignment.Right);
-                    if (bottomRight == null)
-                    {
-                        return;
-                    }
-                }
                 bottomRight.text = value;
             }
         }
@@ -46,23 +32,10 @@ namespace Optimization
         {
             get
             {
-                if (center != null)
-                {
-                    return center.text;
-                }
-                center = CreateLabel("LabelInfoCenter", 32, TextAnchor.LowerCenter, white, Font, TextAlignment.Center);
-                if (center == null)
-                    return "";
                 return center.text;
             }
             set
             {
-                if(center == null)
-                {
-                    center = CreateLabel("LabelInfoCenter", 32, TextAnchor.LowerCenter, white, Font, TextAlignment.Center);
-                    if (center == null)
-                        return;
-                }
                 center.text = value;
             }
         }
@@ -71,23 +44,10 @@ namespace Optimization
         {
             get
             {
-                if (networkStatus != null)
-                {
-                    return networkStatus.text;
-                }
-                networkStatus = CreateLabel("LabelNetworkStatus", 32, TextAnchor.UpperLeft, white, Font, TextAlignment.Left);
-                if (networkStatus == null)
-                    return "";
                 return networkStatus.text;
             }
             set
             {
-                if(networkStatus == null)
-                {
-                    networkStatus = CreateLabel("LabelNetworkStatus", 32, TextAnchor.UpperLeft, white, Font, TextAlignment.Left);
-                    if (networkStatus == null)
-                        return;
-                }
                 networkStatus.text = value;
             }
         }
@@ -96,23 +56,10 @@ namespace Optimization
         {
             get
             {
-                if (topCenter != null)
-                {
-                    return topCenter.text;
-                }
-                topCenter = CreateLabel("LabelInfoTopCenter", 32, TextAnchor.UpperCenter, white, Font, TextAlignment.Center);
-                if (topCenter == null)
-                    return "";
                 return topCenter.text;
             }
             set
             {
-                if(topCenter == null)
-                {
-                    topCenter = CreateLabel("LabelInfoTopCenter", 32, TextAnchor.UpperCenter, white, Font, TextAlignment.Center);
-                    if (topCenter == null)
-                        return;
-                }
                 topCenter.text = value;
             }
         }
@@ -121,23 +68,10 @@ namespace Optimization
         {
             get
             {
-                if (topLeft != null)
-                {
-                    return topLeft.text;
-                }
-                topLeft = CreateLabel("LabelInfoTopLeft", 30, TextAnchor.UpperLeft, white, Font, TextAlignment.Left, FontStyle.Bold);
-                if (topLeft == null)
-                    return "";
                 return topLeft.text;
             }
             set
             {
-                if(topLeft == null)
-                {
-                    topLeft = CreateLabel("LabelInfoTopLeft", 30, TextAnchor.UpperLeft, white, Font, TextAlignment.Left, FontStyle.Bold);
-                    if (topLeft == null)
-                        return;
-                }
                 topLeft.text = value;
             }
         }
@@ -146,23 +80,10 @@ namespace Optimization
         {
             get
             {
-                if (topRight != null)
-                {
-                    return topRight.text;
-                }
-                topRight = CreateLabel("LabelInfoTopRight", 28, TextAnchor.UpperRight, white, Font, TextAlignment.Right);
-                if (topRight == null)
-                    return "";
                 return topRight.text;
             }
             set
             {
-                if(topRight == null)
-                {
-                    topRight = CreateLabel("LabelInfoTopRight", 28, TextAnchor.UpperRight, white, Font, TextAlignment.Right);
-                    if (topRight == null)
-                        return;
-                }
                 topRight.text = value;
             }
         }
@@ -171,23 +92,10 @@ namespace Optimization
         {
             get
             {
-                if (version != null)
-                {
-                    return version.text;
-                }
-                version = CreateLabel("VERSION", 30, TextAnchor.MiddleCenter, white, Font, TextAlignment.Center);
-                if (version == null)
-                    return "";
                 return version.text;
             }
             set
             {
-                if(version == null)
-                {
-                    version = CreateLabel("VERSION", 30, TextAnchor.MiddleCenter, white, Font, TextAlignment.Center);
-                    if (version == null)
-                        return;
-                }
                 version.text = value;
             }
         }
@@ -231,38 +139,125 @@ namespace Optimization
             return text;
         }
 
-        #region Old Version
-        //internal static TextMesh CreateLabel(string name, int size, TextAnchor anchor, Color color, Font font, TextAlignment align)
-        //{
-        //    if (font == null)
-        //        return null;
-        //    GameObject res = GameObject.Find(name);
-        //    if (res == null)
-        //        return null;
-        //    if (res.GetComponent<UILabel>() == null)
-        //        return null;
-        //    TextMesh text = res.GetComponent<TextMesh>();
-        //    if (text == null)
-        //        text = res.AddComponent<TextMesh>();
-        //    MeshRenderer render = res.GetComponent<MeshRenderer>();
-        //    if (render == null)
-        //        render = res.AddComponent<MeshRenderer>();
-        //    res.transform.localScale = new Vector3(4.9f, 4.9f);
-        //    UILabel label = res.GetComponent<UILabel>();
-        //    render.material = font.material;
-        //    text.font = font;
-        //    text.fontSize = size;
-        //    text.anchor = anchor;
-        //    text.alignment = align;
-        //    text.color = color;
-        //    if (label != null)
-        //    {
-        //        text.text = label.text;
-        //        label.enabled = false;
-        //    }
-        //    text.richText = true;
-        //    return text;
-        //}
-        #endregion
+        private static TextMesh CreateShadow(string name, int size, TextAnchor anchor, Color color, Font font, TextAlignment align, FontStyle style = FontStyle.Normal)
+        {
+            if (font == null)
+                return null;
+            GameObject res1 = GameObject.Find(name);
+            if (res1 == null || res1.GetComponent<UILabel>() == null)
+                return null;
+
+            GameObject res = (GameObject)GameObject.Instantiate(res1, res1.transform.position, res1.transform.rotation);
+
+            TextMesh text = res.GetComponent<TextMesh>();
+            if (text == null)
+                text = res.AddComponent<TextMesh>();
+            MeshRenderer render = res.GetComponent<MeshRenderer>();
+            if (render == null)
+                render = res.AddComponent<MeshRenderer>();
+
+            UILabel label = res.GetComponent<UILabel>();
+            render.material = font.material;
+            text.font = font;
+            text.fontSize = size;
+            text.anchor = anchor;
+            text.alignment = align;
+            text.color = color;
+            text.fontStyle = style;
+            res.transform.SetParent(res1.transform.parent);
+            res.transform.localPosition = res1.transform.localPosition;
+            res.transform.localRotation = res1.transform.localRotation;
+            res.transform.localScale = new Vector3(4.9f, 4.9f);
+            Transform tf = text.transform;
+            float deltaX = 1.25f;
+            if(anchor == TextAnchor.MiddleRight || anchor == TextAnchor.UpperRight || anchor == TextAnchor.LowerRight)
+            {
+                deltaX = -deltaX;
+            }
+            float deltaY = 1.25f;
+            if((int)anchor >= 6)
+            {
+                deltaY = -deltaY;
+            }
+                    
+            tf.localPosition = new Vector3(tf.localPosition.x + deltaX, tf.localPosition.y - deltaY, tf.localPosition.z + 0.00001f);
+            
+            if (label != null)
+            {
+                text.text = label.text;
+                label.enabled = false;
+            }
+            res.layer = 5;
+            text.richText = true;
+            return text;
+        }
+
+        private class HUDLabel
+        {
+
+            private TextAlignment align;
+            private TextAnchor anchor;
+            public string Name;
+            private Color color;
+            private TextMesh regular;
+            private TextMesh shadow;
+            private int size;
+            private FontStyle style;
+
+            public string text
+            {
+                get
+                {
+                    if(regular != null)
+                    {
+                        return regular.text;
+                    }
+                    CreateMeshes();
+                    if(regular == null)
+                    {
+                        return string.Empty;
+                    }
+                    return regular.text;
+                }
+                set
+                {
+                    if(regular == null)
+                    {
+                        CreateMeshes();
+                        if(regular == null)
+                        {
+                            return;
+                        }
+                    }
+                    regular.text = value.ToHTMLFormat();
+                    if (Anarchy.Configuration.VideoSettings.ShadowsUI.Value)
+                    {
+                        shadow.text = $"<color=#111111>{value.RemoveHex().RemoveHTML()}</color>";
+                    }
+                }
+            }
+
+
+
+
+            public HUDLabel(string name, int size, TextAnchor anchor, Color color, TextAlignment align, FontStyle style = FontStyle.Normal)
+            {
+                Name = name;
+                this.size = size;
+                this.anchor = anchor;
+                this.color = color;
+                this.align = align;
+                this.style = style;
+            }
+
+            private void CreateMeshes()
+            {
+                if(regular == null)
+                {
+                    regular = CreateLabel(Name, size, anchor, color, Anarchy.UI.Style.Font, align, style);
+                }
+                shadow = CreateShadow(Name, size, anchor, color, Anarchy.UI.Style.Font, align, style);
+            }
+        }
     }
 }
