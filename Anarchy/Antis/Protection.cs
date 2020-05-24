4fabb708c195d778478b4bda7ee5b919e896e3b9
Order <43047529+Orrder@users.noreply.github.com>
2020-05-24 11:04:39 +0300

    Update antis

--- Anarchy/Antis/Protection.cs
+++ Anarchy/Antis/Protection.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Antis.Protections;
+﻿using Antis.Protections;
 
 namespace Antis
 {
     public static class Protection
     {
         public static IProtection<string> HeroAnimationCheck = new HeroAnimationChecker();
-        public static IProtection<string> InstantiationCheck = new EmptyProtection<string>();
+        public static IProtection<string> InstantiationNameCheck = new EmptyProtection<string>();
+        public static IProtection<string> InstantiationSpamCheck = new EmptyProtection<string>();
         public static IProtection<string> RPCNameCheck = new EmptyProtection<string>();
+        public static IProtection<string> RPCSpamheck = new EmptyProtection<string>();
         public static IProtection<string[]> SkinsCheck = new EmptyProtection<string[]>();
         public static IProtection<string> TitanAnimationCheck = new TitanAnimationChecker();
         public static IProtection<string> URLCheck = new URLChecker();
-
     }
 }
---
+++ Anarchy/Antis/Protections/InstantiateSpamChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Antis.Protections
+{
+    public class InstantiateSpamChecker : IProtection<string>
+    {
+        public bool Check(string rpcName)
+        {
+            return true;
+        }
+
+        bool IProtection.Check(object rpcName)
+        {
+            return true;
+        }
+    }
+}
