8490e32c19f0625617c75da2eb8b8c5755486d6d
shigatena <zizdu78@hotmail.fr>
2021-06-03 16:19:19 +0200

    Revert "Test"
    This reverts commit 0acd1f35192ed69e66c4bbe27642c07c83a12754.

--- Anarchy/Cache/RespawnPosition.cs
+++ Anarchy/Cache/RespawnPosition.cs
@@ -1,88 +1,90 @@
-﻿using UnityEngine;
-
-namespace Cache
+﻿namespace Cache
 {
+    using UnityEngine;
 
-    internal static class RespawnPositions
+    namespace Optimization.Caching
     {
-        private static Vector3[] _heroPositions;
-        private static Vector3[] _titanPositions;
-
-        public static void Dispose()
+        internal static class RespawnPositions
         {
-            _heroPositions = null;
-            _titanPositions = null;
-        }
+            private static Vector3[] _heroPositions;
+            private static Vector3[] _titanPositions;
 
-        public static Vector3 RandomHeroPosition
-        {
-            get
+            public static void Dispose()
             {
-                if (_heroPositions != null)
-                {
-                    return _heroPositions[Random.Range(0, _heroPositions.Length)];
-                }
-                GameObject[] gos = GameObject.FindGameObjectsWithTag("playerRespawn");
-                if (gos != null)
-                {
-                    _heroPositions = new Vector3[gos.Length];
-                    for (int i = 0; i < gos.Length; i++)
-                    {
-                        _heroPositions[i] = gos[i].transform.position;
-                    }
-                }
-                if (_heroPositions != null)
-                {
-                    return _heroPositions[Random.Range(0, _heroPositions.Length)];
-                }
-                return Vectors.Zero;
+                _heroPositions = null;
+                _titanPositions = null;
             }
-        }
 
-        public static Vector3 RandomTitanPosition
-        {
-            get
+            public static Vector3 RandomHeroPosition
             {
-                if (_titanPositions != null)
+                get
                 {
-                    return _titanPositions[Random.Range(0, _titanPositions.Length)];
-                }
-                GameObject[] gos = GameObject.FindGameObjectsWithTag("titanRespawn");
-                if (gos != null)
-                {
-                    _titanPositions = new Vector3[gos.Length];
-                    for (int i = 0; i < gos.Length; i++)
+                    if (_heroPositions != null)
                     {
-                        _titanPositions[i] = gos[i].transform.position;
+                        return _heroPositions[Random.Range(0, _heroPositions.Length)];
                     }
+                    GameObject[] gos = GameObject.FindGameObjectsWithTag("playerRespawn");
+                    if (gos != null)
+                    {
+                        _heroPositions = new Vector3[gos.Length];
+                        for (int i = 0; i < gos.Length; i++)
+                        {
+                            _heroPositions[i] = gos[i].transform.position;
+                        }
+                    }
+                    if (_heroPositions != null)
+                    {
+                        return _heroPositions[Random.Range(0, _heroPositions.Length)];
+                    }
+                    return Vectors.Zero;
                 }
-                if (_titanPositions != null)
-                {
-                    return _titanPositions[Random.Range(0, _titanPositions.Length)];
-                }
-                return Vectors.Zero;
             }
-        }
 
-        public static Vector3[] TitanPositions
-        {
-            get
+            public static Vector3 RandomTitanPosition
             {
-                if (_titanPositions != null)
+                get
                 {
-                    return _titanPositions;
-                }
-                GameObject[] gos = GameObject.FindGameObjectsWithTag("titanRespawn");
-                if (gos != null)
-                {
-                    _titanPositions = new Vector3[gos.Length];
-                    for (int i = 0; i < gos.Length; i++)
+                    if (_titanPositions != null)
                     {
-                        _titanPositions[i] = gos[i].transform.position;
+                        return _titanPositions[Random.Range(0, _titanPositions.Length)];
                     }
-                    return _titanPositions;
+                    GameObject[] gos = GameObject.FindGameObjectsWithTag("titanRespawn");
+                    if (gos != null)
+                    {
+                        _titanPositions = new Vector3[gos.Length];
+                        for (int i = 0; i < gos.Length; i++)
+                        {
+                            _titanPositions[i] = gos[i].transform.position;
+                        }
+                    }
+                    if (_titanPositions != null)
+                    {
+                        return _titanPositions[Random.Range(0, _titanPositions.Length)];
+                    }
+                    return Vectors.Zero;
                 }
-                return new Vector3[0];
+            }
+
+            public static Vector3[] TitanPositions
+            {
+                get
+                {
+                    if (_titanPositions != null)
+                    {
+                        return _titanPositions;
+                    }
+                    GameObject[] gos = GameObject.FindGameObjectsWithTag("titanRespawn");
+                    if (gos != null)
+                    {
+                        _titanPositions = new Vector3[gos.Length];
+                        for (int i = 0; i < gos.Length; i++)
+                        {
+                            _titanPositions[i] = gos[i].transform.position;
+                        }
+                        return _titanPositions;
+                    }
+                    return new Vector3[0];
+                }
             }
         }
     }
