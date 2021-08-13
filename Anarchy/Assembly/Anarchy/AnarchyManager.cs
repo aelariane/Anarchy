bbb250a1eaf0f54663d0920c8452a59a8dffad39
Order <43047529+Orrder@users.noreply.github.com>
2021-08-13 16:24:19 +0300

    Update AnarchyManager.cs

--- Anarchy/Assembly/Anarchy/AnarchyManager.cs
+++ Anarchy/Assembly/Anarchy/AnarchyManager.cs
@@ -22,7 +22,7 @@ namespace Anarchy
         //In case if you want to make sync only between YOUR version. Just set CustomName to something that not equals string.Empty or ""
 
         //And AnarchyVersion should match as well in ANY case if you want any kind of sync
-        public static readonly Version AnarchyVersion = new Version("0.9.5.0");
+        public static readonly Version AnarchyVersion = new Version("0.9.5.1");
 
         /// <summary>
         /// Your version Custom name
